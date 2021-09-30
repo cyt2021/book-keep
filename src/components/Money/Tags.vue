12db7f74ffd8291b3dd720ed92d3e0374379538e
qiaomengnan <285921957@qq.com>
2021-09-30 09:28:07 +0800

    消除Tags 警告，目前还有bug

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -6,9 +6,9 @@
       </button>
     </div>
     <ul class="current">
-      <li v-for="tag in dataSource" :key="tag"
+      <li v-for="tag in dataSource" :key="tag.id"
           :class="{selected: selectedTags.indexOf(tag)>=0}"
-          @click="toggle(tag)">{{tag}}
+          @click="toggle(tag)">{{tag.name}}
       </li>
     </ul>
   </div>
