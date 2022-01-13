8d1c2c32e46fe0fa3321066e11eed70982e7af0b
chenyuting <357815153@qq.com>
2022-01-13 16:08:05 +0800

    放在github上预览

---
+++ dist/precache-manifest.407fad0d367a559717b86765d784e122.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7219a7b7c890941658c",
+    "url": "/css/app.26c0af7b.css"
+  },
+  {
+    "revision": "969505204a3b63e0fcb4b6a72b305081",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7219a7b7c890941658c",
+    "url": "/js/app.3622a7fa.js"
+  },
+  {
+    "revision": "a8816f7c60e779525407",
+    "url": "/js/chunk-vendors.ba2b8fee.js"
+  },
+  {
+    "revision": "38c86ee580c0f0227a68bad3d0663039",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
